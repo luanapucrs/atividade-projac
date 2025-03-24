b4376ae9f7fc105fbb969562d504a8dd01d51ede
Vicenzo Frusciante <81040684+VicenzoMF@users.noreply.github.com>
2025-03-24 23:00:50 +0000

    observers implementados

--- DesafioPadroesProjeto-2025-main/Q4_Visualizadores/src/App.java
+++ DesafioPadroesProjeto-2025-main/Q4_Visualizadores/src/App.java
@@ -3,22 +3,36 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         var dados = new FonteDeDados();
-        var cm = new VisualizadorDeMedia(dados.getValores());
-        var cs = new VisualizadorDeSomatorio(dados.getValores());
+        var vm = new VisualizadorDeMedia();
+        var vs = new VisualizadorDeSomatorio();
+        var vp = new VisualizadorDeProduto(); // Novo visualizador
 
         Scanner s = new Scanner(System.in);
         int valor = 0;
-        while(true){
-            System.out.println("\nEntre um valor positivo maior que zero (0=fim):");
+
+        while (true) {
+            System.out.println("\nOpções:");
+            System.out.println("-1 = Ligar Visualizador de Somatório");
+            System.out.println("-2 = Ligar Visualizador de Média");
+            System.out.println("-3 = Ligar Visualizador de Produto");
+            System.out.println("0 = Sair");
+            System.out.println("Ou entre um valor positivo maior que zero:");
+
             valor = Integer.parseInt(s.nextLine());
-            if (valor == 0){
+            if (valor == 0) {
                 break;
+            } else if (valor == -1) {
+                dados.addObserver(vs);
+                System.out.println("Visualizador de Somatório ligado.");
+            } else if (valor == -2) {
+                dados.addObserver(vm);
+                System.out.println("Visualizador de Média ligado.");
+            } else if (valor == -3) {
+                dados.addObserver(vp);
+                System.out.println("Visualizador de Produto ligado.");
+            } else if (valor > 0) {
+                dados.add(valor);
             }
-            dados.add(valor);
-            cs.acrescentaValor(valor);
-            cm.acrescentaValor(valor);
-            cs.exibeSomatorio();
-            cm.exibeMedia();
         }
         System.out.println("Fim");
     }
